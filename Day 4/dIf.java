6f2b6183f5aab8e7bed1b9403c7ab3150ef20f37
Sahil Shah <ss9656484@gmail.com>
2025-01-07 11:22:57 +0530

    if anf if else

---
+++ Day 4/dIf.java
@@ -0,0 +1,10 @@
+// If conditional statement
+
+public class dIf {
+    public static void main(String[] args) {
+        int x = 8;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        }
+    }
+}
