669f3e723fd1a2e84b5b9bc61a25146b512bb977
Sahil Shah <ss9656484@gmail.com>
2025-01-07 10:26:42 +0530

    ternery operator

--- Day 4/TerneryOperator.java
+++ Day 4/TerneryOperator.java
@@ -1,10 +1,11 @@
 /* The ? Operator
-If expression1 is true, then expression2 is evaluated, otherwise, expression3 is evaluated.
+If expression1 is true, then expression2 is evaluated, otherwise, expression3 is evaluated
+here in this eg. (x > y) is expression1, x is expression2 and y is expression3
 */
 public class TerneryOperator {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 5;
+        int x = 8;
+        int y = 16;
         int result = (x > y) ? x : y;
         System.out.println(result);
     }
