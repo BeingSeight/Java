9c8b0163c3fd7ede94b3a92067e433ac028b19d7
Sahil Shah <ss9656484@gmail.com>
2025-01-09 13:05:26 +0530

    missing break

---
+++ Day 5/cMissingBreak.java
@@ -0,0 +1,25 @@
+// In a switch, break statements are optional
+
+public class cMissingBreak {
+    public static void main(String[] args) {
+        for(int i=0;i<12;i++){
+            switch(i) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                System.err.println("i is less than 5");
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                System.err.println("i is less than 10");
+                break;
+                default:
+                System.err.println("i is 10 or more ");
+            }
+        }
+    }
+}
