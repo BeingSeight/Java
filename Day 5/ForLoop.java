8aac487a3ba0740eec2862efdb8045b9211f835c
Sahil Shah <ss9656484@gmail.com>
2025-01-09 12:42:02 +0530

    for loop

---
+++ Day 5/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int a = 1, b = 6;a<b ; a++, b--) {
+            System.out.println("a: " + a + " b: " + b);
+        }
+    }
+}
