df216cb71ea1155844b19be840446640d0bd7df1
Sahil Shah <ss9656484@gmail.com>
2025-01-14 11:02:55 +0530

    2d array

---
+++ Day 6/c2Darray.java
@@ -0,0 +1,19 @@
+// WAP to calculate the sum of 2D array elements given below:
+
+public class c2Darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+        System.out.println("Sum of elements of 2D array is: " + sum);
+    }
+}
