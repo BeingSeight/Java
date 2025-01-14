29310b517987e8dcc5a2cccd320f6564648c485c
Sahil Shah <ss9656484@gmail.com>
2025-01-14 10:32:29 +0530

    array

---
+++ Day 6/aArray.java
@@ -0,0 +1,21 @@
+// write a program to find sum of elements of an array
+import java.util.Scanner;
+
+public class aArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of elements of array is: " + sum);
+        sc.close();
+    }
+}
