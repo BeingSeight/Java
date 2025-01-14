3d778d7303610a07dc4fc53987075d80f608c258
Sahil Shah <ss9656484@gmail.com>
2025-01-14 10:38:07 +0530

    array

--- Day 6/aArray.java
+++ Day 6/aArray.java
@@ -6,15 +6,18 @@ public class aArray {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
         System.out.println("Enter the elements of array: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += arr[i];
         }
+
         System.out.println("Sum of elements of array is: " + sum);
         sc.close();
     }
