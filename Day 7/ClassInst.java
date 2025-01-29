6dc24ceacaed04b9cdafaee2996d0ccccfd907a2
BeingSeight <ss9656484@gmail.com>
2025-01-29 12:54:10 +0530

    class inst

--- Day 7/ClassInst.java
+++ Day 7/ClassInst.java
@@ -1,64 +1,70 @@
-package Class;
-
-
-class ClassInst {
+class Student2{
+    // defining field or data member or instance variable 
     private int id;
     private String name;
     private int marks1;
     private int marks2;
 
-    ClassInst() {
+    Student2() {
         id = 0;
         name = "";
         marks1 = 0;
         marks2 = 0;
     }
-
-    ClassInst(int i, String n, int m1, int m2) {
+    Student2(int i, String n, int m1, int m2){
         id = i;
         name = n;
         marks1 = m1;
         marks2 = m2;
     }
-
-    public String toString() {
-        return "ID: " + id + "\nName: " + name + "\nMarks1: " + marks1 + "\nMarks2: " + marks2;
+    @Override
+    public String toString(){
+        return "Id: " + id + " Name: " + name + " Marks1: " + marks1 + " Marks2: " + marks2;
     }
-
-    public int getId() {
+    public int getId(){
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getMarks1() {
-        return marks1;
-    }
-
-    public int getMarks2() {
-        return marks2;
-    }
-
-    public void setId(int i) {
+    public void setId(int i){
         id = i;
     }
-
-    public void setName(String n) {
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
         name = n;
     }
-
-    public void setMarks1(int m1) {
+    public int getMarks1(){
+        return marks1;
+    }
+    public void setMarks1(int m1){
         marks1 = m1;
     }
-
-    public void setMarks2(int m2) {
+    public int getMarks2(){
+        return marks2;
+    }
+    public void setMarks2(int m2){
         marks2 = m2;
     }
-
-    public void results(){
-        
+    public void result(){
         System.out.println("Total Marks: " + (marks1 + marks2));
+    }
+}
+public class ClassInst{
+    public static void main(String[] args) {
+        Student2 s1 = new Student2();
+        Student2 s2 = new Student2(1, "Ravi", 90, 80);
+        System.out.println(s1);
+        System.out.println(s2);
+        s1.setId(2);
+        s1.setName("Raj");
+        s1.setMarks1(80);
+        s1.setMarks2(70);
+        System.out.println(s1);
+        System.out.println("Id: " + s1.getId());
+        System.out.println("Name: " + s1.getName());
+        System.out.println("Marks1: " + s1.getMarks1());
+        System.out.println("Marks2: " + s1.getMarks2());
+        s1.result();
+        s2.result();
     }
 }
